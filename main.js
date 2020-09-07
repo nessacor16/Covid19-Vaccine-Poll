1ee6e20f62ba1b34c7efada39b6bf2d672ebed8a
Nessa BestA <nessacor16@gmail.com>
2020-09-07 03:03:35 -0400

    update

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 
 // Using MongoClient Class
+const mongodb = require("mongodb");
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
 
-const uri = "mongodb+srv://<titanPlanet>:<Missionpossible16%21>@covid19-vaccinepoll.tcjgy.mongodb.net/< Covid19-VaccinePoll>?retryWrites=true&w=majority";
+
+
+
+const uri = "mongodb+srv://<titanPlanet>:<Missionpossible16%21>@covid19-vaccinepoll.tcjgy.mongodb.net/<Covid19-VaccinePoll>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+  const database = client.db("sample_airbnb")
+  const collection = database.collection("listingsAndReviews");
   client.close();
-  // console.log(result)
+  console.log(collection);
 });
-
-
-
-
-
 
 
 
